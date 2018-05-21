aa2989f1e326cdd2b8f0ab08993459144233739a
deepixel-dev1 <35761445+deepixel-dev1@users.noreply.github.com>
2018-05-21 17:10:05 +0900

    Add New MAKA_API DOxygen

---
+++ makanative/api/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['createinstance',['CreateInstance',['../classdp_1_1android_1_1_d_p_factory_for_android.html#a18a0d6e8d585ad611077ba74df55a69a',1,'dp::android::DPFactoryForAndroid']]]
+];
