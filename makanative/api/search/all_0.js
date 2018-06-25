145aa188e91da4cf059f7a7fabaa415a7a6f2b21
deepixel-dev1 <35761445+deepixel-dev1@users.noreply.github.com>
2018-06-25 17:44:16 +0900

    revision maka api

---
+++ makanative/api/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_5fmaka_5fdata_5f',['_MAKA_DATA_',['../struct___m_a_k_a___d_a_t_a__.html',1,'']]]
+];
---
+++ makanative/api/search/all_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['process',['process',['../structdp_1_1makanative_1_1_i_maka.html#a673e18d78425f72c1a1e57ecdc5f06e9',1,'dp::makanative::IMaka']]]
+];
