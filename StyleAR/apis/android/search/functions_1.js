9c0fd75d8f79a1343a2065460a761106b9ed6fef
deepixel-dev1 <35761445+deepixel-dev1@users.noreply.github.com>
2019-05-20 14:50:24 +0900

    (+) image filter

--- StyleAR/apis/android/search/functions_1.js
+++ StyleAR/apis/android/search/functions_1.js
@@ -3,6 +3,7 @@ var searchData=
   ['setanchorpoint',['setAnchorPoint',['../a00023.html#ae2a5dacc4e1b4dcc2ddbfc3f377bdaec',1,'xyz::deepixel::stylear::DPEarringParam']]],
   ['setbitmap',['setBitmap',['../a00023.html#a6cb129a47bdec5b9cbb26cd39ba03f96',1,'xyz::deepixel::stylear::DPEarringParam']]],
   ['setearringparam',['setEarringParam',['../a00031.html#a33c226a478fe45393d275b610000fb8a',1,'xyz.deepixel.stylear.DPStyleAREarring.setEarringParam()'],['../a00035.html#a98d1e5f0c1c282669c65b8f9769f4f17',1,'xyz.deepixel.stylear.DPStyleAREarringView.setEarringParam()']]],
+  ['setfilterenabled',['setFilterEnabled',['../a00031.html#add8289d4af09ee01218c52d9975474ea',1,'xyz.deepixel.stylear.DPStyleAREarring.setFilterEnabled()'],['../a00035.html#a91b8e4fdf8c8e28080d76ba80104a308',1,'xyz.deepixel.stylear.DPStyleAREarringView.setFilterEnabled()']]],
   ['setleftearringparam',['setLeftEarringParam',['../a00031.html#a490acaeeb195a482be157b65ac2890e2',1,'xyz.deepixel.stylear.DPStyleAREarring.setLeftEarringParam()'],['../a00035.html#ac808eecf42bf2699ed0efc9ef152e71f',1,'xyz.deepixel.stylear.DPStyleAREarringView.setLeftEarringParam()']]],
   ['setrightearringparam',['setRightEarringParam',['../a00031.html#a57b4edda80776070a2ee8578f5eb1def',1,'xyz.deepixel.stylear.DPStyleAREarring.setRightEarringParam()'],['../a00035.html#aa5aa1d8d40ae86448d4f119c8ecebf37',1,'xyz.deepixel.stylear.DPStyleAREarringView.setRightEarringParam()']]],
   ['setsize',['setSize',['../a00023.html#a335be9b00136d7ca83b39fce61fead99',1,'xyz::deepixel::stylear::DPEarringParam']]],
